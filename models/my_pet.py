ed6c18efc406b09eb0a80ef65eec135c2e6c8a9d
JK Long <69000167+BaeWaterLeo@users.noreply.github.com>
2021-08-25 11:11:38 +0700

    Update my_pet.py

--- models/my_pet.py
+++ models/my_pet.py
@@ -11,20 +11,11 @@ class MyPet(models.Model):
     description = fields.Text('Pet Description')
     age = fields.Integer('Pet Age', default=1)
     weight = fields.Float('Weight (kg)')
-    #---------------------------------------------------------------------------------------
-    dob = fields.Date('DOB', required=False)
-    @api.depends('date_of_birth')
-    def _compute_person_age(self):
-        if self.date_of_birth:
-            birth_date = datetime.strptime(self.birth_date, '%m/%d/%Y')
-            self.age = (datetime.today() - birth_date).days / 365
-    #---------------------------------------------------------------------------------------
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')
     ], string='Gender', default='male')
-    species = fields.Many2one(comodel_name'', string='Species')
-    hobbies = fields.Many2many(comodel_name='', string='Hobbies')
+   
     pet_image = fields.Binary("Pet Image", attachment=True, help="Pet Image")    
     owner_id = fields.Many2one('res.partner', string='Owner')
     product_ids = fields.Many2many(comodel_name='product.product', 
@@ -33,11 +24,4 @@ class MyPet(models.Model):
                                 column1='col_pet_id',
                                 column2='col_product_id')
 
-#class SaleOrderLine(models.Model):
-#    _inherit = 'sale.order.line'
-#     x_field = fields.Char('X Field')
-
-class MyPetSpecies(models.Model):
-    _name
-    
     
